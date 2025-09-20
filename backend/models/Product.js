66f50e51b5ab57549d19a521c469e5be1f3b478f
qasimkdr <kdrqasim@gmail.com>
2025-09-20 03:57:11 -0700

    Create Product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  imageUrl: { type: String, required: true },
+  isTrending: { type: Boolean, default: false },
+  category: { type: String, required: true },
+
+});
+
+module.exports = mongoose.model('Product', productSchema);
